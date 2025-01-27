42f2216b686d5f5f5595cabd51d3065c518afb47
llamouth <150949923+llamouth@users.noreply.github.com>
2025-01-26 23:17:16 -0500

    feat: added authentication using clerk

---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='pb-20 px-4'>
+            <div className="">
+                <h1>Manage Your Finances  <br/> With Intelligence</h1>
+                <p>An AI-powered financial management platform that helps you track, analyze, and optimize your spending</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
