ad17e96cf49b38cdf46d406f9038e252cde0215d
llamouth <150949923+llamouth@users.noreply.github.com>
2025-02-01 22:15:24 -0500

    feat: connected database to frontend

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,8 +5,11 @@ import FineLogo from "../public/Fine-Ace-Logo.png";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser()
+
   return (
     <div className="fixed top-0 w-full bg-white/80 shadow-lg z-50 backdrop-blur-md border-b">
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
@@ -18,37 +21,36 @@ const Header = () => {
             alt="Fine-Ace Logo"
           />
         </Link>
+
         <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+              <Button variant='outline'>
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+            <Link href={"/transaction/create"}>
+              <Button className='flex items-center gap-2'>
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
 
-            <SignedIn>
-                <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
-                    <Button variant='outline'>
-                        <LayoutDashboard size={18} />
-                        <span className="hidden md:inline">Dashboard</span>
-                    </Button>
-                </Link>
-                <Link href={"/transaction/create"}>
-                    <Button className='flex items-center gap-2'>
-                        <PenBox size={18} />
-                        <span className="hidden md:inline">Add Transaction</span>
-                    </Button>
-                </Link>
-            </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant='outline'>Login</Button>
+            </SignInButton>
+          </SignedOut>
 
-            <SignedOut>
-                <SignInButton forceRedirectUrl="/dashboard">
-                    <Button variant='outline'>Login</Button>
-                </SignInButton>
-            </SignedOut>
-
-            <SignedIn>
-                <UserButton appearance={{
-                    elements: {
-                        avatarBox: 'w-10 h-10',
-                    }
-                }}/>
-            </SignedIn>
-
+          <SignedIn>
+            <UserButton appearance={{
+              elements: {
+                avatarBox: 'w-10 h-10',
+              }
+            }}/>
+          </SignedIn>
         </div>
       </nav>
     </div>
